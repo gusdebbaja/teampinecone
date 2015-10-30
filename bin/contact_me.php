6ef950134490e1b44ee5bc98f24c3c1877778261
gusdebbaja <gusdebbaja@users.noreply.github.com>
2015-10-31 05:11:30 +0800

    Update contact_me.php

--- bin/contact_me.php
+++ bin/contact_me.php
@@ -1,13 +1,13 @@
 <?php
 // check if fields passed are empty
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
+//if(empty($_POST['name'])  		||
+//   empty($_POST['email']) 		||
+//   empty($_POST['message'])	||
+//   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+//   {
+//	echo "No arguments Provided!";
+//	return false;
+//   }
 
 $name = $_POST['name'];
 $email_address = $_POST['email'];
