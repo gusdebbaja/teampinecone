21311e5ca82003287bfe07bbe0e77dc7e81e5c9a
gusdebbaja <gusdebbaja@users.noreply.github.com>
2015-10-31 04:59:26 +0800

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -23,7 +23,7 @@ $(function() {
 	   firstName = name.split(' ').slice(0, -1).join(' ');
          }
 	 $.ajax({
-                url: "bin/contact_me.php",
+                url: "http://fantasyhashtag.me/bin/contact_me.php",
             	type: "POST",
             	data: {name: name, email: email, message: message},
             	cache: false,
